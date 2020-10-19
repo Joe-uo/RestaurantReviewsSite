3d301a8ef7e8281a5c49325ea174364086e6b8e6
Mark Tyers <aa7401@coventry.ac.uk>
2020-10-19 18:06:54 +0000

    fixed install instructions

--- .guides/content/Project-Setup-b029.md
+++ .guides/content/Project-Setup-b029.md
@@ -19,4 +19,6 @@ Now click on the **Live Site** button to open your website in a second tab.
 
 If the page is blank return to the terminal in Codio where you will see a warning dialog. Agree to the terms and your site will load in the second tab.
 
+![Warning Message](.guides/img/warning.png)
+
 You are now ready to start your assignment.
--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo
-echo "============= CHANGING DIRECTORY TO ${green}FILES${reset} =============="
+echo "========= CHANGING DIRECTORY TO ${green}FILES${reset} =============="
 echo
 if [[ -f "./files" ]]
 then
@@ -22,23 +22,25 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y psmisc lsof tree sqlite3 sqlite3-doc build-essential
 echo
-echo "========== INSTALLING NODE USING ${green}NODESOURCE${reset} =========="
+echo "========= INSTALLING NODE USING ${green}NODESOURCE${reset} ========="
 echo
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
-echo
-echo "===== INSTALLING THE LATEST VERSION OF ${green}NODEJS${reset} ======"
-echo
-nvm install node
-nvm use node
-nvm alias default node
-nvm use default
 echo
 echo "=========== INSTALLING THE ${green}NODE PACKAGES${reset} ==========="
 echo
 npm install
 npm audit fix
+echo
+echo "============== RUNNING THE ${green}UNIT TESTS${reset} =============="
+echo
 npm test
+echo
+echo "================ RUNNING THE ${green}LINTER${reset} ================"
+echo
 npm run linter
+echo
+echo "===== CHECKING THE VERSION OF ${green}NODEJS${reset} INSTALLED ====="
+echo
 node -v
 echo "================= ${green}SETUP COMPLETED ${reset} ================="
